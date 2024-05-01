7d2a05d488e5f541cfa4d1ec53b94f5fd7bb04f5
Toghrul Mukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-05-01 15:05:14 +0200

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/ui/theme-provider";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 export const metadata: Metadata = {
 	title: "NaruDo",
@@ -19,6 +20,7 @@ export default function RootLayout({
 				<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
 					{children}
 					<SpeedInsights />
+					<Analytics />
 				</ThemeProvider>
 			</body>
 		</html>
