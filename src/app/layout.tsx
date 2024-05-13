1d2b160d4bb7da4c2021ee7b70f859bfeee06f90
Toghrul Mukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-05-13 02:45:05 +0200

    html body 100%

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,8 +32,8 @@ export default function RootLayout({
 				},
 			}}
 		>
-			<html lang="en">
-				<body>
+			<html lang="en" className="h-full">
+				<body className="h-full">
 					<ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
 						{children}
 						<SpeedInsights />
