8c5ace680fa568493605f6255f226124be28df9f
Toghrul Mukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-05-13 02:02:50 +0200

    temporary dev shit update

--- src/app/(dev-shit-temporary)/theme/page.tsx
+++ src/app/(dev-shit-temporary)/theme/page.tsx
@@ -1,43 +1,71 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { useState } from "react";
+import { useTheme } from "next-themes";
 
 export default function Home() {
-	const [light, setLight] = useState<boolean>(true);
-	const borderShit = "border-blue-800 text-blue-800 rounded-3xl p-2 border";
-	const sizes = "h-32 w-44";
+	const { setTheme } = useTheme();
+	const className = "border-blue-800 text-blue-800 rounded-3xl p-2 border text-sm size-32";
 	return (
-		<div className={cn("flex h-screen flex-col gap-8", light && "bg-white", !light && "bg-black")}>
+		<div className={cn("flex h-full flex-col", "bg-background")}>
 			<div className="flex gap-4 border-b-8 border-blue-500 p-1 px-4">
-				<button className=" text-blue-500" onClick={() => setLight(true)}>
+				<button className=" font-bold text-blue-500 dark:font-normal" onClick={() => setTheme("light")}>
 					light
 				</button>
-				<button className=" text-blue-500" onClick={() => setLight(false)}>
+				<button className=" text-blue-500 dark:font-bold" onClick={() => setTheme("dark")}>
 					dark
 				</button>
+				<div className="ml-6 flex gap-4">
+					<p>1. good</p>
+					<p>2. cursed</p>
+					<p>3. all_colors</p>
+				</div>
 			</div>
+			<div className="flex flex-wrap items-center gap-4 border-b-8 border-blue-500 p-4 *:w-72 ">
+				<Component back="background" fore="foreground" />
+				<Component back="card" fore="card-foreground" />
+				<Component back="popover" fore="popover-foreground" />
+				<Component back="primary" fore="primary-foreground" />
+				<Component back="secondary" fore="secondary-foreground" />
+				<Component back="muted" fore="muted-foreground" />
+				<Component back="accent" fore="accent-foreground" />
+				<Component back="destructive" fore="destructive-foreground" />
+			</div>
+			{/* <div className="flex flex-wrap items-center gap-4 border-b-8 border-blue-500 p-4 *:w-72 ">
+				<h2>cursed</h2>
+				<Component back="background" fore="muted-foreground" />
+				<Component back="primary" fore="muted-foreground" />
+				<Component back="secondary" fore="muted-foreground" />
+			</div> */}
 			<div className="flex flex-wrap gap-4 p-6">
-				<div className={cn(sizes, borderShit, "bg-background")}>--background</div>
-				<div className={cn(sizes, borderShit, "bg-foreground")}>--foreground</div>
-				<div className={cn(sizes, borderShit, "bg-card")}>--card</div>
-				<div className={cn(sizes, borderShit, "bg-card-foreground")}>--card-foreground</div>
-				<div className={cn(sizes, borderShit, "bg-popover")}>--popover</div>
-				<div className={cn(sizes, borderShit, "bg-popover-foreground")}>--popover-foreground</div>
-				<div className={cn(sizes, borderShit, "bg-primary")}>--primary</div>
-				<div className={cn(sizes, borderShit, "bg-primary-foreground")}>--primary-foreground</div>
-				<div className={cn(sizes, borderShit, "bg-secondary")}>--secondary</div>
-				<div className={cn(sizes, borderShit, "bg-secondary-foreground")}>--secondary-foreground</div>
-				<div className={cn(sizes, borderShit, "bg-muted")}>--muted</div>
-				<div className={cn(sizes, borderShit, "bg-muted-foreground")}>--muted-foreground</div>
-				<div className={cn(sizes, borderShit, "bg-accent")}>--accent</div>
-				<div className={cn(sizes, borderShit, "bg-accent-foreground")}>--accent-foreground</div>
-				<div className={cn(sizes, borderShit, "bg-destructive")}>--destructive</div>
-				<div className={cn(sizes, borderShit, "bg-destructive-foreground")}>--destructive-foreground</div>
-				<div className={cn(sizes, borderShit, "bg-border")}>--border</div>
-				<div className={cn(sizes, borderShit, "bg-input")}>--input</div>
-				<div className={cn(sizes, borderShit, "bg-ring")}>--ring</div>
+				<div className={cn(className, "bg-background")}>--background</div>
+				<div className={cn(className, "bg-foreground")}>--foreground</div>
+				<div className={cn(className, "bg-card")}>--card</div>
+				<div className={cn(className, "bg-card-foreground")}>--card-foreground</div>
+				<div className={cn(className, "bg-popover")}>--popover</div>
+				<div className={cn(className, "bg-popover-foreground")}>--popover-foreground</div>
+				<div className={cn(className, "bg-primary")}>--primary</div>
+				<div className={cn(className, "bg-primary-foreground")}>--primary-foreground</div>
+				<div className={cn(className, "bg-secondary")}>--secondary</div>
+				<div className={cn(className, "bg-secondary-foreground")}>--secondary-foreground</div>
+				<div className={cn(className, "bg-muted")}>--muted</div>
+				<div className={cn(className, "bg-muted-foreground")}>--muted-foreground</div>
+				<div className={cn(className, "bg-accent")}>--accent</div>
+				<div className={cn(className, "bg-accent-foreground")}>--accent-foreground</div>
+				<div className={cn(className, "bg-destructive")}>--destructive</div>
+				<div className={cn(className, "bg-destructive-foreground")}>--destructive-foreground</div>
+				<div className={cn(className, "bg-border")}>--border</div>
+				<div className={cn(className, "bg-input")}>--input</div>
+				<div className={cn(className, "bg-ring")}>--ring</div>
 			</div>
+		</div>
+	);
+}
+
+function Component({ back, fore }: { back: string; fore: string }) {
+	return (
+		<div className={`bg-${back} border p-4`}>
+			<p className={`text-${fore} text-center`}>{`${back} .. ${fore}`}</p>
 		</div>
 	);
 }
