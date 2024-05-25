fcd1f58cff8e06c3792b83d6948c93701ebf27cf
Toghrul Mukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-05-25 16:41:35 +0200

    disable logger

--- src/drizzle/db.ts
+++ src/drizzle/db.ts
@@ -4,4 +4,4 @@ import { drizzle } from "drizzle-orm/vercel-postgres";
 import { sql } from "@vercel/postgres";
 import * as schema from "./schema";
 
-export const db = drizzle(sql, { schema, logger: true });
+export const db = drizzle(sql, { schema });
