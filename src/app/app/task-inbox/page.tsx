6d6d4074a3e16890ec620e09245d4f6818faf7db
Toghrul Mukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-05-25 12:11:43 +0200

    commit

--- src/app/app/task-inbox/page.tsx
+++ src/app/app/task-inbox/page.tsx
@@ -1,28 +1,6 @@
 "use client";
 
-import {
-	ArrowDownNarrowWide,
-	ArrowUpNarrowWide,
-	CheckCheck,
-	Ellipsis,
-	Inbox,
-	Slash,
-	SlidersHorizontal,
-	Tag,
-	ToggleLeft,
-	ToggleRight,
-} from "lucide-react";
-import { Button, buttonVariants } from "@/components/ui/button";
-import { Plus } from "lucide-react";
-import { cn } from "@/lib/utils";
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import * as React from "react";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
-
-import { Card, CardContent } from "@/components/ui/card";
-import { Separator } from "@/components/ui/separator";
-import Link from "next/link";
 import { useQuery } from "@tanstack/react-query";
 import { getProjects } from "@/app/_serverActions/queries";
 import { useRouter } from "next/navigation";
