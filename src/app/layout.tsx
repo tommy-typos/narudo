88e689bd7bfe8b7034e1fe114c7f28968f815e9a
Toghrul Mukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-05-26 01:07:39 +0200

    quick deneme to revert later

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,13 @@ import { dark } from "@clerk/themes";
 import { ReactQueryProvider } from "@/lib/reactQueryProvider";
 import { Toaster } from "@/components/ui/toaster";
 
+function stringifyDate(date: Date) {
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, "0"); // getMonth() returns 0-11, so we add 1
+	const day = String(date.getDate()).padStart(2, "0"); // getDate() returns the day of the month
+
+	return `${year}-${month}-${day}`;
+}
 export const metadata: Metadata = {
 	title: "NaruDo",
 	description: "Black Belt Your Productivity",
@@ -22,10 +29,10 @@ export default function Layout({
 }>) {
 	return (
 		<ClerkProvider
-			signInForceRedirectUrl={"/app/today"}
-			signInFallbackRedirectUrl={"/app/today"}
-			signUpForceRedirectUrl={"/app/today"}
-			signUpFallbackRedirectUrl={"/app/today"}
+			signInForceRedirectUrl={`/app/date/${stringifyDate(new Date())}`}
+			signInFallbackRedirectUrl={`/app/date/${stringifyDate(new Date())}`}
+			signUpForceRedirectUrl={`/app/date/${stringifyDate(new Date())}`}
+			signUpFallbackRedirectUrl={`/app/date/${stringifyDate(new Date())}`}
 			signInUrl={"/sign-in"}
 			signUpUrl={"/sign-up"}
 			appearance={{
