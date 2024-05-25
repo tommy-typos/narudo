66c57f734aa22604afa9f3d3755385e63e5b390f
Toghrul Mukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-05-26 01:29:31 +0200

    saga city

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,6 +1,10 @@
+"use client";
+
+import { stringifyDate } from "@/components/client/addTask";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-	redirect("/app/today");
+	redirect(`/app/date/${stringifyDate(new Date())}`);
+	// TODO ::: learn if it is better to user router.push here (for the sake of useQuery issue)
 	return <></>;
 }
