f25c95c1a58d4e7ef1ebc6c516e19d6b889e2b29
Toghrul Mukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-05-26 22:24:13 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
 	return (
 		<>
 			<div className="mb-12 flex w-screen justify-center">
-				<div className="m-10 flex w-4/5 max-w-[1200px] items-center justify-between">
+				<div className="m-10 flex w-2/3 max-w-[900px] items-center justify-between">
 					<div className="flex text-xl">
 						<p className={protestRevolution.className}>naru</p>
 						<div className="text-narudorange">
@@ -22,10 +22,10 @@ export default function Home() {
 						</div>
 					</div>
 					<div className="flex items-center gap-6">
-						<Link href="/guides" className="px-3">
+						{/* <Link href="/guides" className="px-3">
 							Guides
 						</Link>
-						<LanguageToggle />
+						<LanguageToggle /> */}
 						<ThemeToggle />
 					</div>
 				</div>
