61eb2b0c2621b2f56bdbec9aa66d37eb1c21d560
Toghrul Mukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-06-15 07:03:17 +0200

    commit

--- src/app/app/friends/page.tsx
+++ src/app/app/friends/page.tsx
@@ -5,6 +5,7 @@ import { QueryKey, useQuery, useQueryClient } from "@tanstack/react-query";
 import { getFriends } from "@/app/_serverActions/queries";
 import { useRouter } from "next/navigation";
 import { useFriendsQuery } from "@/lib/queries";
+import { FriendsSvg } from "@/lib/svgs/svgExporter";
 
 export default function Home() {
 	const friendsQuery = useFriendsQuery();
@@ -20,7 +21,8 @@ export default function Home() {
 	return (
 		<>
 			{friendsQuery.data && friendsQuery.data.length === 0 && (
-				<div className="py-4">
+				<div className="flex flex-col items-center justify-center py-4">
+					<FriendsSvg className="max-h-72" />
 					<p className="text-center">Your friend list is current empty.</p>
 					<p className="text-center">Add your friends now to start collaborating 🙂</p>
 				</div>
--- src/lib/svgs/svgExporter.ts
+++ src/lib/svgs/svgExporter.ts
@@ -1,2 +1,3 @@
 export { default as NarutoBeltSvg } from "./naruto-belt.svg";
 export { default as TeamGoalsSvg } from "./undraw_team_goals.svg";
+export { default as FriendsSvg } from "./undraw_friends.svg";
