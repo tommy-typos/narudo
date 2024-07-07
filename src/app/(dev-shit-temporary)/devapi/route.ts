d73e8705d0b6b8d424c358723319afc2b5e1235a
Toghrul Mukhtarov <137985975+tommy-typos@users.noreply.github.com>
2024-07-07 15:08:32 +0200

    commit

--- src/app/(dev-shit-temporary)/devapi/route.ts
+++ src/app/(dev-shit-temporary)/devapi/route.ts
@@ -46,42 +46,6 @@ export async function GET(request: Request) {
 			// ======================================================================================================== //
 			// ======================================================================================================== //
 			// ======================================================================================================== //
-			// 		ninjaScore: sql`${users.ninjaScore} + 10`,
-			// data = await addNewTask({
-			// 	task: {
-			// 	  "date": "2024-05-19",
-			// 	  "id": "a9d699e3-e113-4d04-8cfe-aef9d47ca14b",
-			// 	  createdAt: new Date(),
-			// 	  "title": "naemoftask",
-			// 	  "description": "",
-			// 	  "time": null,
-			// 	  "isTogether": true,
-			// 	  "isAssignedToSb": false
-			// 	},
-			// 	"assignees": [
-			// 	  "user_2geNcwNuJb3gMrUW2YePEvdybXf",
-			// 	  "user_2geT4BCtRGPRSqwabjAzQ9vahGp"
-			// 	],
-			// 	"project": {
-			// 	  "projectId": "a9d699e3-e113-4d04-8cfe-aef9d47ca14b",
-			// 	  "subCatId": "5284a9b7-4f09-451c-8395-7f0f598d907f"
-			// 	}
-			//   })
-			// const clerkUsers = await clerkClient.users.getUserList();
-			// data =  clerkUsers.data.map((item) => ({
-			// 	userId: item.id,
-			// 	userName: item.username,
-			// 	firstName: item.firstName,
-			// 	lastName: item.lastName,
-			// 	fullName: item.fullName,
-			// 	imageUrl: item.imageUrl,
-			// 	hasImage: item.hasImage,
-			// }));
-			// data = await getTasksBySubCategory('5d8a9e54-a962-49ae-b145-a28d4662c58d');
-			// data = await getTasksBySubCategory('a70e7615-27a3-43c0-a397-0788cc770088'); // task inbox > default
-			// data = await getOverdueTasks(new Date());
-			// data = await getNotificationCount();
-			// data = await saveNoteAndRetrieve();
 			// ======================================================================================================== //
 			// ======================================================================================================== //
 			// ======================================================================================================== //
